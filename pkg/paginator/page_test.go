7851254cd12f86f246e4474b485efc763b0bfebf
Tom Dunlap <tom@motevets.com>
2020-04-25 20:39:52 -0400

    WIP

---
+++ pkg/paginator/page_test.go
@@ -0,0 +1,36 @@
+package paginator_test
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	. "github.com/alcoano/alcobinder/pkg/paginator"
+)
+
+pageText := `
+**First** line
+
+_second_ line
+`
+
+var _ = Describe("Page", func() {
+
+	Describe(".NewPageFromMarkdown", func() {
+		var page Page
+		BeforeEach(func() {
+			page = NewPageFromMarkdown(1, pageText)
+		})
+
+		It("adds the page number", func(){
+			Expect(page.PageNumber).To(Equal("1"))
+		})
+
+		It("adds the markdown", func(){
+			Expect(page.Markdown).To(Equal(pageText))
+		})
+
+		It("adds HTML generated from the markdown", func(){
+			Expect(page.HTML).To(Equal("<p><strong>First</strong> line</p>\n\n<p><em>Second</em> line</p>\n"))
+		})
+	})
+})
